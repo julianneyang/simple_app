67717bc0adc0f6a86c72602d274d9d75b7af5fbe
Julianne <julianneyangcollege@gmail.com>
2025-09-04 17:18:24 -0400

    new manifest

--- src/00_REPOS_SETUP.R
+++ src/00_REPOS_SETUP.R
@@ -2,8 +2,17 @@ renv::init(bioconductor="3.20")
 renv::install("rsconnect")
 renv::snapshot()
 rsconnect::writeManifest(
+  appFiles= files,
   #appFileManifest = "src/app.R",
   appMode= "shiny",
   envManagementR = TRUE,
   verbose = TRUE
 )
+
+
+files <- list.files(
+  path = "results", 
+  recursive = TRUE,   # look into subfolders
+  full.names = TRUE   # keep relative paths
+)
+
