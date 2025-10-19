f09fb9cb58ba8f400ca6aa6f55b87b7d5fc5abaa
Julianne <julianneyangcollege@gmail.com>
2025-10-19 12:44:30 -0400

    fixing breaking change

--- app.R
+++ app.R
@@ -971,14 +971,7 @@ server <- function(input, output) {
   output$plot_slc_indo_1 <- renderPlot({ print(plot_slc_indo_1) })
   output$plot_slc_indo_2 <- renderPlot({ print(plot_slc_indo_2) })
   
-  output$plot_se_supp_1 <- renderPlot({ print(plot_se_supp_1) })
-  output$plot_se_supp_2 <- renderPlot({ print(plot_se_supp_2) })
-  output$plot_se_supp_3 <- renderPlot({ print(se_histo_ctrl) })
-  output$plot_se_supp_4 <- renderPlot({ print(se_histo_indo) })
-  output$plot_se_supp_5 <- renderPlot({ print(control_icpms) })
-  output$plot_se_supp_6 <- renderPlot({ print(se_supp_icpms) })
-  output$plot_se_supp_7 <- renderPlot({ print(control_fitc) })
-  output$plot_se_supp_8 <- renderPlot({ print(se_supp_fitc) })
+
   
   output$plot_icp_1 <- renderPlot({ print(icpms) })
   
@@ -992,7 +985,8 @@ server <- function(input, output) {
   })
   
   observeEvent(input$tabs, {
-    if (input$tabs == "SE_Supp") {
+    if (input$tabs == "SE Supplementation") {
+      
       se_indo <- read.csv(here("data/phenotype/SE_Supp/SE_Supp_Indomethacin_DAI.csv"))
       se_indo_ctrl <- se_indo %>% filter(Diet =="Control")
       plot_se_supp_1 <- plot_avg_trajectory(se_indo_ctrl, title_string = "Indomethacin", subtitle_string = "Control Diet; HET p = 0.5727, MUT p = 0.5037")
@@ -1028,6 +1022,14 @@ server <- function(input, output) {
       
       control_fitc <- plot_fitc(here("data/phenotype/SE_Supp/Control_FITC_Running_Total_2025Oct.csv"), "FITC", "Control Diet; HET p = 0.3787, MUT p = 0.9314") 
       
+      output$plot_se_supp_1 <- renderPlot({ print(plot_se_supp_1) })
+      output$plot_se_supp_2 <- renderPlot({ print(plot_se_supp_2) })
+      output$plot_se_supp_3 <- renderPlot({ print(se_histo_ctrl) })
+      output$plot_se_supp_4 <- renderPlot({ print(se_histo_indo) })
+      output$plot_se_supp_5 <- renderPlot({ print(control_icpms) })
+      output$plot_se_supp_6 <- renderPlot({ print(se_supp_icpms) })
+      output$plot_se_supp_7 <- renderPlot({ print(control_fitc) })
+      output$plot_se_supp_8 <- renderPlot({ print(se_supp_fitc) })
     }
   })
   
